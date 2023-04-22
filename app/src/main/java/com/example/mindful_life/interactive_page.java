62266671bd49f5fa8778000428273efb9cf3d39a
Joshua Mbanugo <joshuambanugo@gmail.com>
2023-04-22 18:33:52 +0100

    Added Comment of what the page needs

--- app/src/main/java/com/example/mindful_life/interactive_page.java
+++ app/src/main/java/com/example/mindful_life/interactive_page.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class interactive_page extends AppCompatActivity {
 
+    /*
+    Page will last 5 seconds
+    Loading page will have a sound with an animation of the logo.
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactive_page);
+
     }
 }
